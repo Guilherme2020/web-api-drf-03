8eb0d4d9c5734f15282da385738fae9c812d2906
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-06-17 15:04:55 -0300

    configuracao de throttle class

--- feedit/settings.py
+++ feedit/settings.py
@@ -45,14 +45,23 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 5,
+        'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
-
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/hour',
+        'user': '50/hour',
+        'api-token': '1/hour',
+    },
 }
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- feeds/views.py
+++ feeds/views.py
@@ -6,6 +6,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.generics import GenericAPIView
 from rest_framework.reverse import reverse
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.authtoken.models import Token
 
 #from feedit.feeds.serializers import
 from .models import *
@@ -17,7 +19,13 @@ from .serializers import ProfileSerializer, PostSerializer, PostSerializerDetail
 from rest_framework.authtoken.views import obtain_auth_token
 
 class CustomAuthToken(ObtainAuthToken):
+
+    throttle_scope = 'api-token'
+    throttle_classes = (ScopedRateThrottle, )
+
+
     def post(self,request,*args,**kwargs):
+
         serializer  = self.serializer_class(data=request.data,
                                             context={
                                                 'request': request
